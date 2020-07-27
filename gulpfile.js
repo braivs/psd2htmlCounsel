fa465434da4cdaa88bc82a6afe12b818f709c8ec
briws <briws@pm.me>
2020-07-27 09:26:50 +0300

    gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const pug = require('gulp-pug');
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
 function style () {
     return gulp.src('./src/sass/**/*.sass')
-            .pipe(sass())
+            .pipe(sourcemaps.init())
+            .pipe(sass().on('error', sass.logError))
+            .pipe(autoprefixer())
+            .pipe(sourcemaps.write('./'))
             .pipe(gulp.dest('./docs/css'))
             .pipe(browserSync.stream())
 }
 
-// function gulp () {
-//     return gulp.src('./src/*.pug')
-//     .pupe(pug()
-// }
+function pugCompile () {
+    return gulp.src('./src/*.pug')
+    .pipe(pug({
+        pretty: true
+    }))
+    .pipe(gulp.dest('./docs/'))
+    .pipe(browserSync.stream())
+}
 
 function watch () {
     browserSync.init({
@@ -21,8 +31,12 @@ function watch () {
         }
     })
     gulp.watch('./src/sass/**/*.sass', style);
-    gulp.watch('./docs/*.html').on('change', browserSync.reload);
+    gulp.watch('./src/*.pug', pugCompile);
+    // gulp.watch('./docs/*.html').on('change', browserSync.reload); //for clean html only
 }
 
-exports.style = style;
-exports.watch = watch; 
+gulp.task('default', function () {
+    watch(); //for gulp
+})
+
+exports.watch = watch; //for gulp watch
