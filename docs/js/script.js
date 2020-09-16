511e04752f00f1b59ae7b9f595d0ae7b82bc5600
briws <briws@pm.me>
2020-09-16 14:24:58 +0300

    scrollToTop update

--- docs/js/script.js
+++ docs/js/script.js
@@ -14,3 +14,23 @@ var mySwiper = new Swiper('.swiper-container', {
         prevEl: pr,
     },
 });
+
+//Get the button:
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 896 || document.documentElement.scrollTop > 896) {
+    mybutton.style.display = "block";
+    } else {
+    mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
