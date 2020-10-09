2544db61aac88290a61a4987910b9629c6f79c48
briws <briws@pm.me>
2020-10-09 13:24:20 +0300

    +burger menu for resolution -767px

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,19 @@
+//изменение значка бургер меню + показ и скрытие бургер меню
+let burgerIconSymbol = document.querySelector('.burgerIconSymbol');
+let sectionHeader__nav = document.querySelector('.section-header__nav');
+burgerIconSymbol.onclick = function(event) {
+    if (burgerIconSymbol.innerHTML === '☰') {
+        burgerIconSymbol.innerHTML = "✕";
+    } else {
+        burgerIconSymbol.innerHTML = "☰";
+    }
+    if (getComputedStyle(sectionHeader__nav).display === "none") {
+        sectionHeader__nav.style.display = "flex";
+    } else {
+        sectionHeader__nav.style.display = "none";
+    }
+};
+
 var pr = document.getElementsByClassName("swiper-prev")[0];
 var nx = document.getElementsByClassName("swiper-next")[0];
 var mySwiper = new Swiper('.swiper-container', {
@@ -19,15 +35,15 @@ var mySwiper = new Swiper('.swiper-container', {
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > offset || document.documentElement.scrollTop > 896) {
-    mybutton.style.display = "block";
-    } else {
-    mybutton.style.display = "none";
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > offset || document.documentElement.scrollTop > 896) {
+//     mybutton.style.display = "block";
+//     } else {
+//     mybutton.style.display = "none";
+//     }
+// }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
